4890b193d1474d848f7f0f99172c4ae04b3f0393
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2020-08-30 14:27:51 +0300

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import QuizQuestion from './QuizQuestion'
 import QuizEnd from './QuizEnd'
-import { resetHistory } from 'sinon'
 
 let quizData = require('./quiz_data.json')
 
