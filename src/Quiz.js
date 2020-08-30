dabee02dd86cc6b95de9995d54af441b9edbc69b
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2020-08-30 15:32:45 +0300

    Update Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -19,6 +19,12 @@ class Quiz extends Component {
         })
     }
 
+    handleResetClick(){
+        this.setState((state) => {
+            return { quiz_position: 1 }
+        })
+    }
+
     render() {
         const isQuizEnd = (this.state.quiz_position - 1) === quizData.quiz_questions.length
         return (
